8e74d03bf7cba7fb6ec7c81a5677465f6784557c
Andreln <andrenicolaysen@gmail.com>
2017-01-25 16:46:42 +0100

    Update nrf_led_button_service.js

--- nrf_led_button_service.js
+++ nrf_led_button_service.js
@@ -1,21 +1,20 @@
 'use strict'
 
 const serviceUUID = '00001523-1212-efde-1523-785feabcd123';
-const buttonCharacteristicUUID = '00001524-1212-efde-1523-785feabcd123';
-const ledCharacteristicUUID = '00001525-1212-efde-1523-785feabcd123';
+const txCharacteristicUUID = '00001524-1212-efde-1523-785feabcd123';
+const rxCharacteristicUUID = '00001525-1212-efde-1523-785feabcd123';
 
 var bleDevice;
 var bleServer;
 var bleService;
-var button1char;
-var ledChar;
-var button1count = 0;
-var toggleFlag = false;
+var txchar;
+var rxchar;
 
 window.onload = function(){
   document.querySelector('#connect').addEventListener('click', connect);
   document.querySelector('#disconnect').addEventListener('click', disconnect);
-  document.querySelector('#led2').addEventListener('click', toggleLED);
+  document.querySelector('#COMMAND_1').addEventListener('click', COMMAND_1);
+  document.querySelector('#COMMAND_2').addEventListener('click', COMMAND_2);
 };
 
 function connect() {
@@ -39,22 +38,22 @@ function connect() {
     log('Got bleService');
     bleService = service;
   })
-  .then(() => bleService.getCharacteristic(buttonCharacteristicUUID))
+  .then(() => bleService.getCharacteristic(txCharacteristicUUID))
   .then( characteristic => {
-    log('Got button1characteristic');
-    button1char = characteristic;
-    return button1char.startNotifications();
+    log('Got txcharacteristic');
+    txchar = characteristic;
+    return txchar.startNotifications();
   })
   .then(() => {
     log('Notifications enabled');
-    button1char.addEventListener('characteristicvaluechanged',handleNotifyButton1);
+    txchar.addEventListener('characteristicvaluechanged',handleNotifyButton1);
   })
   .then(() => {
-    return bleService.getCharacteristic(ledCharacteristicUUID);
+    return bleService.getCharacteristic(txCharacteristicUUID);
   })
   .then( characteristic => {
-    ledChar = characteristic;
-    log('Got ledChar');
+    rxchar = characteristic;
+    log('Got rxChar');
   })
   .catch(error => {
     log('> connect ' + error);
@@ -75,23 +74,12 @@ function disconnect() {
   }
 }
 
-function handleNotifyButton1(event) {
-  button1count += 1;
-  log('Notification triggered by Button 1 ' + button1count);
-  document.getElementById("btn1").innerHTML = button1count;
+function COMMAND_1(){
+
 }
 
-function toggleLED(){
-    let toggle;
-    if(toggleFlag === true){
-      toggle = new Uint8Array([0]);
-      toggleFlag = false;
-    }
-    else{
-      toggle = new Uint8Array([1]);
-      toggleFlag = true;
-    }
-    return ledChar.writeValue(toggle);
+function COMMAND_2(){
+
 }
 
 
