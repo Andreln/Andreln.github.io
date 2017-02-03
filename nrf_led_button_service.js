d96a24a4de8442233ad4e4e911927da6f94f323f
André Nicolaysen <andrenicolaysen@gmail.com>
2017-02-03 16:20:17 +0100

    Adding data receive function

--- nrf_led_button_service.js
+++ nrf_led_button_service.js
@@ -107,9 +107,16 @@ function getValue(){
 function DATARECEIVED(event){
 	let value = event.target.value;
 	value = value.buffer ? value : new DataView(value);
-	//let receivedData0 = value.getUint8(0);
-	log(value.length);
-
+	let data0 = value.getUint8(0);
+	let data1 = value.getUint8(1); 
+	let data2 = value.getUint8(2); 
+	let data3 = value.getUint8(3); 
+	let data4 = value.getUint8(4); 
+	let data5 = value.getUint8(5);
+	let data5 = value.getUint8(6);
+	let data5 = value.getUint8(7);
+	
+	log('['data0']''['data1']''['data2']''['data3']''['data4']''['data5']''['data6']''['data7']')
 }
 
 function log(text) {
