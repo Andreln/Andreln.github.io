fd97ad2e8b2f7a3d37ffe4bd8fd9f3c92d56b4f1
André Nicolaysen <andrenicolaysen@gmail.com>
2017-02-08 18:27:07 +0100

    sup acc workz yo

--- js-saves.js
+++ js-saves.js
@@ -21,3 +21,49 @@ function dmHdlr(aX, aY, aZ) {
     //s$('aY').innerHTML = aY ? aY.toFixed(3) : '?';
     s$('aZ').innerHTML = aZ ? aZ.toFixed(3) : '?';
 }
+
+
+
+
+
+	function toggleAcc(){
+		if(startAcc==false){
+			log('GOO!');
+			startAcc=true;
+		}
+		else {
+			startAcc=false;
+			log('STAAAHP');
+		}
+	}
+
+	//------------- Accessing and showing sensordata from phone ------------- // 
+	do{
+		var s$ = function(e) {return document.getElementById(e);};
+
+		if (window.DeviceMotionEvent) {
+			window.addEventListener('devicemotion', function(ev) {
+				var acc = ev.acceleration;
+				dmHdlr(acc.z);
+			}, false);
+		}
+		else {
+			log("devicemotion not supported on your device");
+		}
+
+		var lastDM = new Date().getTime();
+
+		function dmHdlr(aZ) {
+			var currDM = new Date().getTime();
+			lastDM = currDM;
+		
+			s$('aZ').innerHTML = aZ ? aZ.toFixed(3) : '?';
+		
+			myLineChart.data.datasets[0].data.shift();
+			myLineChart.data.datasets[0].data.push(aZ);
+			myLineChart.data.labels = [(i+0), (i+1), (i+2), (i+3), (i+4), (i+5), (i+6), (i+7), (i+8), (i+9)];
+			i++;
+			myLineChart.update();
+		}
+	}
+	while(startAcc==true);
