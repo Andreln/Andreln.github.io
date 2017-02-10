5733f5fca61398c4f1d83cad6be89bb963167da9
André Nicolaysen <andrenicolaysen@gmail.com>
2017-02-10 15:28:37 +0100

    Setting up toggling of site views

--- main.js
+++ main.js
@@ -99,11 +99,14 @@ window.onload = function(){
 }
 
 //------------- TOGGLE VISIBILITY ------------- //
-function toggle_visibility(id) {
-	var div = document.getElementById(id);
-	if(div.style.display == 'block')
-		div.style.display = 'none';
-	else
-		div.style.display = 'block';
+function toggle_visibility(showId, hideId1, hideId2) {
+	let showDiv = document.getElementById(showId);
+  let hideDiv1 = document.getElementById(hideId1);
+  let hideDiv2 = document.getElementById(hideId2);
+
+  showDiv.style.display = 'block'
+	hideDiv1.style.display = 'none';
+	hideDiv2.style.display = 'none';
+
 }
 //------------- TOGGLE VISIBILITY ------------- //
