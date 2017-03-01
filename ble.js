3f86266dff1f9cee0c6545c3de002fdd882e5d23
André Nicolaysen <andrenicolaysen@gmail.com>
2017-03-01 16:36:15 +0100

    Adding mpu service and characteristics

--- ble.js
+++ ble.js
@@ -6,8 +6,6 @@ const rxUUID = '6e400003-b5a3-f393-e0a9-e50e24dcca9e';
 const txUUID = '6e400002-b5a3-f393-e0a9-e50e24dcca9e';
 
 const MPU_Service_UUID = '6e409999-b5a3-f393-e0a9-e50e24dcca9e'
-                        //6E40F00D-B5A3-F393-E0A9-E50E24DCCA9E
-
 const acc_Characteristics_UUID = '6e408888-b5a3-f393-e0a9-e50e24dcca9e';
 
 var bleDevice;
@@ -41,8 +39,11 @@ function connect() {
 	}
 
 	log('Requesting Bluetooth Device...');
-	navigator.bluetooth.requestDevice({filters: [ {services: [serviceUUID, MPU_Service_UUID]} ] } )
-	.then(device => {
+	navigator.bluetooth.requestDevice({
+    filters: [ {services: [serviceUUID]} ],
+    optionalServices: [MPU_Service_UUID]
+  })
+  .then(device => {
       bleDevice = device;
 
       // Adding event listener to detect loss of connection
