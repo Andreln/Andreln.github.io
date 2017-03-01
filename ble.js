cc70c16c0fec2f749befdd6d2259563bd1364e0c
André Nicolaysen <andrenicolaysen@gmail.com>
2017-03-01 14:55:30 +0100

    Updating site

--- ble.js
+++ ble.js
@@ -1,7 +1,6 @@
-'use strict'
-
-
-
+//
+//    BLE Connection for Resonator
+//
 const serviceUUID = '6e400001-b5a3-f393-e0a9-e50e24dcca9e';
 const rxUUID = '6e400003-b5a3-f393-e0a9-e50e24dcca9e';
 const txUUID = '6e400002-b5a3-f393-e0a9-e50e24dcca9e';
@@ -14,19 +13,19 @@ var txCharacteristics;
 
 
 window.onload = function(){
-  document.querySelector('#connect').addEventListener('click', connect);
-  document.querySelector('#disconnect').addEventListener('click', disconnect);
+  document.querySelector('#connectBtn').addEventListener('click', connect);
+  document.querySelector('#disconnectBtn').addEventListener('click', disconnect);
   document.querySelector('#refresh').addEventListener('click', disconnect);
-
-  document.querySelector('#ON').addEventListener('click', disconnect);				// CHANGE!
-  document.querySelector('#OFF').addEventListener('click', disconnect);				// CHANGE!
-
-  document.querySelector('#refresh').addEventListener('click', disconnect);			// CHANGE!
+  //
+  // document.querySelector('#ON').addEventListener('click', disconnect);				// CHANGE!
+  // document.querySelector('#OFF').addEventListener('click', disconnect);				// CHANGE!
+  //
+  // document.querySelector('#refresh').addEventListener('click', disconnect);			// CHANGE!
 
 }
 
 function connect() {
-  log('Does this even work?');
+  'use strict'
 	if (!navigator.bluetooth) {
 	  log('Web Bluetooth API is not available.\n' +
 		  'Please make sure the Web Bluetooth flag is enabled.');
@@ -34,57 +33,62 @@ function connect() {
 	}
 
 	log('Requesting Bluetooth Device...');
-	navigator.bluetooth.requestDevice({filters: [{services: [serviceUUID]}]})  // {filters: [{services: [serviceUUID]}]}
+	navigator.bluetooth.requestDevice({filters: [{services: [serviceUUID]}]})
 	.then(device => {
-	bleDevice = device;
-	log('Gonnecting to GATT server... ');
-	return device.gatt.connect();
+      bleDevice = device;
+
+      // Adding event listener to detect loss of connection
+      bluetoothDevice.addEventListener('gattserverdisconnected', disconnectHandler);
+      log('> Found' + bluetoothDevice.name);
+      log('Connecting to GATT Server...');
+
+       // Connect to gattserver
+      return device.gatt.connect();
+      log('> Bluetooth Device connected: ');
 	})
 
-	.then(server => {
-	bleServer = server;
-	log('Got GATT server... ');
-	log('Getting service... ');
-	return server.getPrimaryService(serviceUUID);
-	})
+  	.then(server => {
+  	bleServer = server;
+  	log('Got GATT server... ');
+  	log('Getting service... ');
+  	return server.getPrimaryService(serviceUUID);
+  	})
 
-	.then(service => {
-	log('Got service... ');
-	bleService = service;
-	log('Getting characteristic... ');
-	return bleService.getCharacteristic(rxUUID);
-	})
+  	.then(service => {
+  	log('Got service... ');
+  	bleService = service;
+  	log('Getting characteristic... ');
+  	return bleService.getCharacteristic(rxUUID);
+  	})
 
-	.then( characteristic => {
-	log('Got rxCharacteristic... ');
-	rxCharacteristics = characteristic;
-	return rxCharacteristics.startNotifications();
-	})
+  	.then( characteristic => {
+  	log('Got rxCharacteristic... ');
+  	rxCharacteristics = characteristic;
+  	return rxCharacteristics.startNotifications();
+  	})
 
-	.then(() => {
-	log('Notifications enabled... ');
-	rxCharacteristics.addEventListener('characteristicvaluechanged',DATARECEIVED);
-	})
+  	.then(() => {
+  	log('Notifications enabled... ');
+  	rxCharacteristics.addEventListener('characteristicvaluechanged',DATARECEIVED);
+  	})
 
-	.then(() => {
-	return bleService.getCharacteristic(txUUID);
-	})
+  	.then(() => {
+  	return bleService.getCharacteristic(txUUID);
+  	})
 
-	.then( characteristic => {
-	txCharacteristics = characteristic;
-	log('Got txCharacteristics...');
-	log('Connected...');
-	})
+  	.then( characteristic => {
+  	txCharacteristics = characteristic;
+  	log('Got txCharacteristics...');
+    buttonToggle('Connect', 'Disconnect')
+  	log('Connected...');
+  	})
 
-	.catch(error => {
-	log('> connect ' + error);
-	});
+  	.catch(error => {
+  	log('> connect ' + error);
+  	});
 }
 
 function disconnect() {
-	// toggle_visibility('Connect');
-	// toggle_visibility('Disconnect');
-
 	if (!bleDevice) {
 	log('No Bluetooth Device connected...');
 	return;
--- viewToggle.js
+++ viewToggle.js
@@ -8,3 +8,12 @@ function viewToggle(showId, hideId1, hideId2) {
 	hideDiv1.style.display = 'none';
 	hideDiv2.style.display = 'none';
 }
+
+
+function buttonToggle(buttonShowId, buttonHideId) {
+	let buttonShow = document.getElementById(buttonShowId);
+	let buttonHide = document.getElementById(buttonHideId);
+
+  buttonShow.style.display = 'block'
+	buttonHide.style.display = 'none';
+}
