13735cb12d0b4dbc0f64f40286ad29168ea400bd
André Nicolaysen <andrenicolaysen@gmail.com>
2017-04-04 16:39:09 +0200

    test

--- views.js
+++ views.js
@@ -10,28 +10,29 @@ function buttonToggle(buttonShowId, buttonHideId) {
 
 function toggleDiv(divId){
 	let div = document.getElementById(divId);
-	
+
 	if(div.style.display !== 'none') {
 		div.style.display ='none';
 	}
 	else{
 		div.style.display = 'block';
 	}
+
+	log('Does this work?');
 }
 
 
 function View(currentPage) {
-	
+
 	let div = document.getElementsByClassName("View");
-	
+
 	for(i = 0; i<div.length; i++) {
-				
+
 		if(currentPage !== div[i].id) {
 			div[i].style.display = 'none';
 		}
 		else {
 			div[i].style.display = 'block';
 		}
-	}	
+	}
 }
-
